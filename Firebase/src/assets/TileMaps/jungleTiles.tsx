f8329eb93d106be681ab895b527d42c439ebfd8d
jjsalvador <doctorjj10@gmail.com>
2020-05-18 23:47:54 -0400

    Added Game
    Added base functionality for game mechanics and gameplay, added new assets (tilesets, tilemaps, sprites), moved game sprites to separate asset folder in src

---
+++ Firebase/src/assets/TileMaps/jungleTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="jungle" tilewidth="64" tileheight="64" tilecount="9" columns="3">
+ <image source="../spritesheets/JungleTileSet.png" width="192" height="192"/>
+</tileset>
--- Firebase/src/main.js
+++ Firebase/src/main.js
@@ -1,202 +1,106 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 1100,
-    height: 700,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+class main extends Phaser.Scene {
+    constructor() {
+        super("LoadGame");
     }
-};
 
-var player;
-var stars;
-var bombs;
-var platforms;
-var cursors;
-var score = 0;
-var gameOver = false;
-var scoreText;
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('sky', '../../assets/Backgrounds/Sky Background.png');
-    this.load.image('ground', '../../assets/platform.png');
-    this.load.image('star', '../../assets/star.png');
-    this.load.image('bomb', '../../assets/Sprites/Scorpion Static.png');
-    this.load.spritesheet('dude', '../../assets/Sprites/SunWukong.png', { frameWidth: 64, frameHeight: 64 });
-}
-
-function create ()
-{
-    //  A simple background for our game
-    this.add.image(400, 300, 'sky');
-
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = this.physics.add.staticGroup();
-
-    //  Here we create the ground.
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    platforms.create(400, 700, 'ground').setScale(3.5).refreshBody();
-
-    //  Now let's create some ledges
-    platforms.create(600, 500, 'ground');
-    platforms.create(50, 400, 'ground');
-    platforms.create(750, 320, 'ground');
-
-    // The player and its settings
-    player = this.physics.add.sprite(100, 450, 'dude');
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-
-    //  Our player animations, turning, walking left and walking right.
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 20, end: 23 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 16, end: 19 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'jump',
-        frames: [ {key: 'dude',  frame: 24 } ],
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'die',
-        frames: [ {key: 'dude',  frame: 30 } ],
-        frameRate: 10
-    });
-
-    //  Input Events
-    cursors = this.input.keyboard.addKeys(
-        {up:Phaser.Input.Keyboard.KeyCodes.SPACE,
-        down:Phaser.Input.Keyboard.KeyCodes.S,
-        left:Phaser.Input.Keyboard.KeyCodes.A,
-        right:Phaser.Input.Keyboard.KeyCodes.D,
-        }
-    );
-
-    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
-
-    stars.children.iterate(function (child) {
-
-        //  Give each star a slightly different bounce
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-    });
-
-    bombs = this.physics.add.group();
-
-    //  The score
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-    //  Collide the player and the stars with the platforms
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.collider(bombs, platforms);
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    this.physics.add.overlap(player, stars, collectStar, null, this);
-
-    this.physics.add.collider(player, bombs, hitBomb, null, this);
-}
-
-function update ()
-{
-    if (gameOver)
-    {
-       this.add.text(350, 200, 'GAME OVER', {fontSize: '64px', fill: '#623'});
-        player.body.enable = false;
+    preload() {
+        // Load in spritesheets
+        this.load.image("jungleTiles", "../assets/Images/JungleTileSet.png", {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+        this.load.spritesheet("Monkey", "../assets/Sprites/SunWukong.png", {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+        this.load.spritesheet("Scorpion", "../assets/Sprites/Scorpion.png", {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+        this.load.image("Staff", "../assets/Sprites/Staff.png", {
+            frameWidth: 32,
+            frameHeight: 4
+        });
         
     }
 
-    if (cursors.left.isDown && !gameOver)
-    {
-        player.setVelocityX(-200);
+    create() {
+        // Add text
+        this.add.text(20, 20, "World 1-1");
+        this.scene.start("World1-1");
 
-        player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown && !gameOver)
-    {
-        player.setVelocityX(200);
-
-        player.anims.play('right', true);
-    }
-    else if (!gameOver)
-    {
-        player.setVelocityX(0);
-
-        player.anims.play('turn');
-    }
-
-    if (cursors.up.isDown && player.body.touching.down && !gameOver)
-    {
-        player.setVelocityY(-350);
-    }
-}
-
-function collectStar (player, star)
-{
-    star.disableBody(true, true);
-
-    //  Add and update the score
-    score += 10;
-    scoreText.setText('Score: ' + score);
-
-    if (stars.countActive(true) === 0)
-    {
-        //  A new batch of stars to collect
-        stars.children.iterate(function (child) {
-
-            child.enableBody(true, child.x, 0, true, true);
-
+        // Create animations
+        this.anims.create({
+            key: "idle_right",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 0,
+                end: 7
+            }),
+            frameRate: 10,
+            repeat: -1 //Use -1 for infinite loops
         });
-
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = false;
-
+        this.anims.create({
+            key: "idle_left",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 8,
+                end: 15
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "run_right",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 16,
+                end: 19
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "run_left",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 20,
+                end: 23
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "jump_right",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 24,
+                end: 24
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "jump_left",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 25,
+                end: 25
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "attack_right",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 26,
+                end: 26
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "attack_left",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 27,
+                end: 27
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
     }
-}
-
-function hitBomb (player, bomb)
-{
-    player.setTint(0xff0000);
-    gameOver = true;
-    player.anims.play('die', true);
 }
