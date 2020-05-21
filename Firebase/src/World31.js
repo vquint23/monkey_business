8c2b47da6b39d46ea34071000457a7703d3ad787
ilikepie21k <jacob.joseph.1@stonybrook.edu>
2020-05-21 18:49:14 -0400

    Added remaining music files and start of world 3-1

---
+++ Firebase/src/World31.js
@@ -0,0 +1,286 @@
+class World31 extends Phaser.Scene {
+    constructor() {
+        super("World3-1");
+    }
+
+    preload() {
+        // Import tile map
+        this.load.image('sky', "../assets/Backgrounds/Sky Background.png");
+        this.load.tilemapCSV('world31', "../assets/TileMaps/world31.csv");
+    }
+
+    create() {
+        paused = false;
+        gotGate = false;
+        wintext = this.add.text(350, 150, 'LEVEL COMPLETE', {fontSize: '64px', fill: '#000'});
+        wintext.setVisible(false);
+
+        // Used to determine which way player is facing
+        left = false;
+
+        // Create tilemap and background
+        bg = this.add.tileSprite(0, 0, this.cameras.main.width / 2, this.cameras.main.height, "sky").setOrigin(0, 0);
+        let scaleX = this.cameras.main.width / bg.width
+        let scaleY = this.cameras.main.height / bg.height
+        let scale = Math.max(scaleX, scaleY)
+        bg.setScale(scale).setScrollFactor(0)
+        map = this.make.tilemap({key: "world31", tileWidth: 64, tileHeight: 64});
+        tileset = map.addTilesetImage("world3tiles");
+        layer = map.createStaticLayer(0, tileset);
+
+        // Set tile blocks to be collidable
+        map.setCollisionBetween(0, 10000, true);
+
+        // Create Player
+        player = this.physics.add.sprite(this.game.config.width/2, 2000, "Monkey");
+        player.body.setSize(45, 60);
+        player.body.setOffset(12, 0);
+
+        // Create Staff
+        staff = this.add.sprite(player.x-64, player.y, "Staff");
+        staff.setOrigin(0, 0);
+
+        // Used as the hitbox of the staff
+        hit = this.physics.add.sprite(player.x - 64, player.y, "Hit");
+        hit.setOrigin(0.5,0.5);
+        hit.setVisible(false);
+        hit.body.setAllowGravity(false);
+
+        // Create Enemies
+        var scorpionA = this.physics.add.sprite(1728, 2176, "Scorpion");
+        var scorpionB = this.physics.add.sprite(2432, 2816, "Scorpion");
+        var scorpionC = this.physics.add.sprite(1241, 1764, "Scorpion");
+        var scorpionD = this.physics.add.sprite(2329, 1190, "Scorpion");
+        var scorpionE = this.physics.add.sprite(3186, 2737, "Scorpion");
+        var scorpionF = this.physics.add.sprite(3931, 2786, "Scorpion");
+        var scorpionG = this.physics.add.sprite(4632, 489, "Scorpion");
+
+        scorpions = this.physics.add.group();
+        scorpions.add(scorpionA);
+        scorpions.add(scorpionB);
+        scorpions.add(scorpionC);
+        scorpions.add(scorpionD);
+        scorpions.add(scorpionE);
+        scorpions.add(scorpionF);
+        scorpions.add(scorpionG);
+
+        // Create Gate (Tiled Location * 64)
+        gate = this.add.sprite(6280, 192, "Gate");
+
+        // Set collision between player, enemies, and collidable layer
+        layer.setCollisionByProperty({collides: true});
+        this.physics.add.collider(player, layer);
+        this.physics.add.collider(scorpions, layer);
+        this.physics.add.collider(scorpions, player, this.takeDamage, null);
+        
+        this.physics.add.overlap(hit, scorpions, this.hitEnemy, null, this);
+        this.physics.add.overlap(player, gate, this.levelWin, null); 
+        
+        // Set up camera that follows player
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(player);
+
+        // Map keyboard inputs for movement to ASD and Space 
+        cursorKeys = this.input.keyboard.createCursorKeys();
+        cursorKeys = this.input.keyboard.addKeys ({
+            up: Phaser.Input.Keyboard.KeyCodes.SPACE,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D, 
+            pause: Phaser.Input.Keyboard.KeyCodes.ENTER
+         });
+
+        }
+
+    extendStaff() {
+        // Controls staff extension by checking for left click
+        if (this.game.input.mousePointer.isDown) {
+            var mouseX = this.game.input.mousePointer.x;
+            var mouseY = this.game.input.mousePointer.y;
+            var camX = this.cameras.main.scrollX;
+            var camY = this.cameras.main.scrollY;
+
+            //Determine if left or right attack animation should play
+            if ((mouseX+ camX) >= player.x) { 
+                left = false;
+                player.play("attack_right", true);
+            } else {
+                left = true;
+                player.play("attack_left", true);
+            }
+            // Change direction of sprite when pointer is clicked while on left or right of it.
+            var angle = Phaser.Math.Angle.Between(player.x, player.y, mouseX + camX, mouseY + camY);
+            staff.setAngle(Phaser.Math.RadToDeg(angle));
+
+            var hitOffsetX = (mouseX + camX) - player.x;
+            var hitOffsetY = (mouseY + camY) - player.y;
+            //Change hitbox of staff offset - radius of 250
+
+            if (hitOffsetX < -250){
+                hitOffsetX = -250;
+            }
+            if (hitOffsetX > 250){
+                hitOffsetX = 250;
+            }
+            if (hitOffsetY < -250){
+                hitOffsetY = -250;
+            }
+            if (hitOffsetY > 250){
+                hitOffsetY = 250;
+            }
+            hit.setOffset(hitOffsetX, hitOffsetY);
+
+            //resize staff
+            this.tweens.add({
+                targets: staff,
+                scaleX: 8,
+                scaleY: 3,
+                duration: 30,
+                repeat: 0
+            });
+        }
+
+        // When mouseup, pull back staff
+        else{
+            this.tweens.add({
+                targets: staff,
+                scaleX: 1,
+                scaleY: 1,
+                angleX: 0,
+                angleY: 0,
+                duration: 30,
+                repeat: 0
+            });
+            // reset staff hitbox
+            hit.setOffset(0, 0);
+        }
+    }
+    
+    hitEnemy(hit, enemy) {
+        enemy.destroy();
+    }
+
+    takeDamage(){
+
+    }
+
+    levelWin(){
+        
+    }
+
+    update() {
+        // Reset player velocity back to 0 every frame
+        player.setVelocityX(0);
+        staff.x = player.x;
+        staff.y = player.y;
+        hit.x = player.x;
+        hit.y = player.y;
+        // Controls movement of player sprite
+        this.movePlayerManager();
+        // Controls scorpion movement
+        this.moveScorpionManager();
+        //Controls pause 
+        this.pauseManager();
+        // Controls Staff Mechanic
+        this.extendStaff();
+
+    }
+
+    movePlayerManager() {
+        // Moves player according to buttons pressed and updates sprite states
+        if (cursorKeys.left.isDown) {
+            left = true;
+            player.setVelocityX(-300);
+            if (player.body.onFloor()) {
+                player.play("run_left", true);
+            } 
+        } else if (cursorKeys.right.isDown) {
+            left = false;
+            player.setVelocityX(300);
+            if (player.body.onFloor()) {
+                player.play("run_right", true);
+            }
+        } else {
+            if (left) {
+                player.play("idle_left", true);
+            } else {
+                player.play("idle_right", true);
+            }
+        }
+        if (cursorKeys.up.isDown && player.body.onFloor())
+        {
+            player.setVelocityY(-525);
+        }
+        if (!player.body.onFloor()) {
+            if (left) {
+                player.play("jump_left", true);
+            } else {
+                player.play("jump_right", true);
+            }
+        }
+    }
+
+    moveScorpionManager(){
+        Phaser.Actions.Call(scorpions.getChildren(), child => {
+            child.body.moves = true;
+            if (player.x < child.x){
+                child.play("scorpion_idle_left", true);
+                child.setVelocityX(-100);
+            }
+            else {
+                child.play("scorpion_idle_right", true);
+                child.setVelocityX(100);
+            }
+            if (child.body.onFloor() && player.y < child.y){
+                child.setVelocityY(-500);
+             }
+        });
+        
+    } 
+
+    pauseManager(){
+        if(cursorKeys.pause.isDown && !paused){
+            //this.scene.sleep();
+            //open pause scene
+        }
+        if (cursorKeys.pause.isDown && paused){
+            //close pause scene
+            //this.scene.wake();
+        }
+        this.paused = !paused;
+    }
+}
+
+var paused;
+var cursorKeys;
+var scorpions;
+var gate;
+var gotGate;
+var wintext;
+var player;
+var bg;
+var map;
+var tileset;
+var layer;
+var staff;
+var left;
+var hit;
+
+var config = {
+    parent: "game-container",
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 700,
+    bgColor: 0x000000,
+    scene: [main3, World31],
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade:{
+            gravity: { y: 700 },
+            debug: false
+        }
+    },
+    autoRound: false
+}
+var game = new Phaser.Game(config);
---
+++ Firebase/src/main3.js
@@ -0,0 +1,145 @@
+class main3 extends Phaser.Scene {
+    constructor() {
+        super("LoadGame");
+    }
+
+    preload() {
+        // Load in spritesheets
+        this.load.image("world3tiles", "../assets/Images/CloudTileSet.png", {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+        this.load.spritesheet("Monkey", "../assets/Sprites/SunWukong.png", {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+        this.load.spritesheet("Scorpion", "../assets/Sprites/Scorpion.png", {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+        this.load.image("Staff", "../assets/Sprites/Staff.png", {
+            frameWidth: 32,
+            frameHeight: 4
+        });
+        this.load.image("Gate", "../assets/Images/gate.png", {
+            frameWidth: 128,
+            frameHeight: 128
+        });
+        this.load.image("Hit", "assets/Images/hit.png", {
+            frameWidth: 20,
+            frameHeight: 20
+        });
+        
+    }
+
+    create() {
+        // Add text
+        this.add.text(20, 20, "World 3-1");
+        this.scene.start("World3-1");
+
+        // Create animations
+        this.anims.create({
+            key: "idle_right",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 0,
+                end: 7
+            }),
+            frameRate: 10,
+            repeat: -1 //Use -1 for infinite loops
+        });
+        this.anims.create({
+            key: "idle_left",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 8,
+                end: 15
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "run_right",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 16,
+                end: 19
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "run_left",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 20,
+                end: 23
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "jump_right",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 24,
+                end: 24
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "jump_left",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 25,
+                end: 25
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "attack_right",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 26,
+                end: 26
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "attack_left",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 27,
+                end: 27
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "hurt_right",
+            frames: this.anims.generateFrameNumbers("Monkey", {
+                start: 28,
+                end: 28
+            }),
+            duration: 1000,
+            frameRate: 10,
+            repeat: -1
+        });
+
+
+        // Create enemy animations
+        this.anims.create({
+            key: "scorpion_idle_left",
+            frames: this.anims.generateFrameNumbers("Scorpion", {
+                start: 0,
+                end: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "scorpion_idle_right",
+            frames: this.anims.generateFrameNumbers("Scorpion", {
+                start: 4,
+                end: 7
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+    }
+}
